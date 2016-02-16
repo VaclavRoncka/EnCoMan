4b770c1b942b17702b06c1d6d9d24adc6867e614
Václav Rončka <vaclav.roncka@gmail.com>
2016-02-16 21:37:47 +0100

    New DbModel created

--- EnCoMan/Ecm.DbLayer/Class1.cs
+++ EnCoMan/Ecm.DbLayer/Class1.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ecm.DbLayer
 {
+    
+
     public class Class1
     {
+        public enum EnergyType : byte { Jedna, dva }
+
+        public void Metod()
+        {
+            //using (var ctx = new EnCoManEntities())
+            //{
+            //    var user = ctx.Users.Where(u => u.Id == 5);
+            //}
+        }
+        
     }
 }
