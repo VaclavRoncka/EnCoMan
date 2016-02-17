d75c07c48a29df1a7801860a079bfd8ec7e65710
Václav Rončka <vaclav.roncka@gmail.com>
2016-02-17 20:46:10 +0100

    First run (Wpf is comunicationg directly to the DB layer!!! refactore it!)

---
+++ EnCoMan/Ecm.DbLayer/DbManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecm.DbLayer
+{
+    
+
+    public class DbManager
+    {
+        
+
+        public static void AddUser()
+        {
+            using (var ctx = new Entities())
+            {
+                var newUser = new User() {UserName = "Shipo"};
+                ctx.Users.Add(newUser);
+
+                ctx.SaveChanges();
+            }
+        }
+        
+    }
+}
