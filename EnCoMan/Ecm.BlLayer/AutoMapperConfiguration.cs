13e5195fff20c07898466668cf49d70492cc0339
Václav Rončka <vaclav.roncka@gmail.com>
2016-02-18 19:34:57 +0100

    Automapper added to BlLayer

---
+++ EnCoMan/Ecm.BlLayer/AutoMapperConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AutoMapper;
+using Ecm.Common.Dto;
+using Ecm.DbLayer;
+
+namespace Ecm.BlLayer
+{
+    public static class AutoMapper
+    {
+        public static IMapper Instance { get; private set; }
+        
+        static AutoMapper()
+        {
+            Instance = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Configuration, ConfigurationDto>();
+                c.CreateMap<User, UserDto>();
+                c.CreateMap<EnergyType, EnergyTypeDto>();
+                c.CreateMap<Periodicity, PeriodicityDto>();
+            }).CreateMapper();
+        }
+    }
+
+    
+
+
+
+}
---
+++ EnCoMan/Ecm.BlLayer/ConfigurationLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Ecm.Common.Dto;
+using Ecm.DbLayer;
+
+namespace Ecm.BlLayer
+{
+    public class ConfigurationLogic
+    {
+        public static ICollection<ConfigurationDto> GetConfigurationList()
+        {
+            var confList = DbManager.GetConfigurations(1);
+            return AutoMapper.Instance.Map<List<ConfigurationDto>>(confList); 
+        }
+    }
+}
+
