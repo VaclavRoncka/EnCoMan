effda6ef656a2cea83f122a233994587fc376aff
Václav Rončka <vaclav.roncka@gmail.com>
2016-02-20 14:52:31 +0100

    Added Web api - Configuration services

--- EnCoMan/Ecm.Web.UiLayer/Controllers/ConfigurationController.cs
+++ EnCoMan/Ecm.Web.UiLayer/Controllers/ConfigurationController.cs
@@ -16,5 +16,37 @@ namespace Ecm.Web.UiLayer.Controllers
         {
             return ConfigurationLogic.GetConfigurationList();
         }
+
+        [Route("Api/Configuration/GetConfiguration")]
+        public ConfigurationDto GetConfiguration(int configurationId)
+        {
+            return ConfigurationLogic.GetConfiguration(configurationId);
+        }
+
+        [Route("Api/Configuration/UpdateConfiguration")]
+        public void UpdateConfiguration(ConfigurationDto configuration)
+        {
+            ConfigurationLogic.UpdateConfiguration(configuration);
+        }
+
+        [Route("Api/Configuration/DeleteConfiguration")]
+        public void DeleteConfiguration(int configurationId)
+        {
+            ConfigurationLogic.DeleteConfiguration(configurationId);
+        }
+
+        [Route("Api/Configuration/CreateConfiguration")]
+        public ConfigurationDto CreateConfiguration(ConfigurationDto configuration)
+        {
+            return ConfigurationLogic.CreateConfiguration(configuration);
+        }
+
+        [Route("Api/ResetDatabaseData")]
+        public void ResetDatabaseData()
+        {
+            ConfigurationLogic.ResetDatabaseData();
+        }
+
+
     }
 }
